eff2d813fa8498aad34160743d1341e2dd0ddf62
Schooner <mba2139@tc.columbia.edu>
2018-05-02 20:37:15 -0400

    UPDATE: EdTech Advisor - Has comments about each page (You'll probably need to focus on SignUp page, SuggestActivity page, & the objectviewholder page.

---
+++ app/src/main/java/com/example/schooner/edtechadvisor/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.example.schooner.edtechadvisor;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private FirebaseAuth mrAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailEditText = (EditText) findViewById(R.id.edit_text_email);
+        passwordEditText = (EditText) findViewById(R.id.edit_text_password);
+        mrAuth = FirebaseAuth.getInstance();
+    }
+
+
+    public void goToSignUp(View view) {
+        Intent goToSignUp = new Intent(this, MainActivity.class);
+        startActivity(goToSignUp);
+    }
+
+    public void logIn(View view)
+    {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        mrAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (!task.isSuccessful())
+                        {
+                            Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(LoginActivity.this, task.getResult().getUser().getEmail() + " logged in successfully",
+                                    Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                });
+    }
+}
